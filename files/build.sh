4c0236602938b745dc3d0203988921335119860c
Manas <3097018+ManasJayanth@users.noreply.github.com>
2022-10-25 20:43:22 +0530

    wip

---
+++ files/build.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+
+HOST_FLAGS=
+
+if [ "$cur__os" = "windows" ]; then
+    HOST_FLAGS="--host x86_64-w64-mingw32"
+fi
+
+find ./ -exec touch -t 200905010101 {} +
+env PATH=/usr/x86_64-w64-mingw32/sys-root/mingw/bin:$PATH ./configure --enable-fat --enable-cxx --prefix=$cur__install $HOST_FLAGS --with-pic",
